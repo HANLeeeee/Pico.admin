d7925a5d65278db0327969aca857f6bf06f4483e
HANLeeeee <hanleeeee@naver.com>
2023-12-08 02:37:17 +0900

    Initial Commit

---
+++ pico.admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pico.admin
+//
+//  Created by 최하늘 on 12/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
